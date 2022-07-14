a7a0e264b7ab14315842bc9bab84b8028f4a0a20
mampek <mampek@gmail.com>
2022-07-14 09:24:00 +0100

    Arrays practise

---
+++ LearningJavaFundamentals/src/ArrayOfObjects.java
@@ -0,0 +1,10 @@
+public class ArrayOfObjects {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3};
+
+        Book[] books = new Book[10];
+
+        books[4] = new Book("A good read", "Matt Greencrofrt", 17.99);
+        
+    }
+}
