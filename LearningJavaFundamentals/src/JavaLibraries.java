ad2d8865abe931376c6d82a21df7fb046d1156e5
mampek <mampek@gmail.com>
2022-07-26 23:36:04 +0100

    Practice with decimals used for money representation

--- LearningJavaFundamentals/src/JavaLibraries.java
+++ LearningJavaFundamentals/src/JavaLibraries.java
@@ -2,6 +2,8 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -31,8 +33,15 @@ public class JavaLibraries {
         LocalDate someDate = LocalDate.of(2022, 11, 16);
         LocalTime someTime = LocalTime.from(anotherDateAndTime);
 
-        System.out.println(theDateAndTime);
+        System.out.println(theDateAndTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")));
         System.out.println(someTime);
+
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        LocalDate myDateOfBirth = LocalDate.of(1975, 8, 19);
+        Period difference = Period.between(myDateOfBirth, yesterday);
+
+        System.out.println("There are " + difference.getYears() + " years and  " + difference.getMonths()
+        + " months and " + difference.getDays() + " days, between two dates.");
 
     }
 }
